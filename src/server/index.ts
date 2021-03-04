760355dcfab902cdf28452ce261f458a9976970f
Jovana <jvujic997@gmail.com>
2021-03-04 12:56:00 +0100

    add cors support

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,6 @@
 import bodyParser from 'body-parser';
 import express from 'express';
+import cors from 'cors';
 
 import { createApi } from './api';
 
@@ -8,6 +9,7 @@ export function CreateServer() {
     const app = express();
     app.disable("x-powered-by");
     app.use(bodyParser.json());
+    app.use(cors())
 
 
     app.use('/api', createApi());
