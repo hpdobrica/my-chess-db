6a39a3302274f45f26e56ae4e82b154eb8c960e0
Dobrica <dobrica@getpenta.com>
2021-03-29 09:11:07 +0200

    api updates attachprofile

--- src/server/api/persons.ts
+++ src/server/api/persons.ts
@@ -10,7 +10,7 @@ export function createRouter(): Router {
     Persons.use(requireAuthMiddleware)
     Persons.get('/', handler(personHandler.getHandlers().getAll));
     Persons.get('/:id', handler(personHandler.getHandlers().getById));
-    Persons.get('/:id/games', )
+    Persons.get('/:id/games', handler(personHandler.getHandlers().getPersonsGames))
 
     Persons.put('/:id/profiles/:profile', handler(personHandler.postHandlers().attachProfile));
     Persons.delete('/:id/profiles/:profile', handler(personHandler.postHandlers().detachProfile));
